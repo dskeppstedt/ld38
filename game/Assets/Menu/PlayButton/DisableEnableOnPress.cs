e183f5ffecc39c3b491b05f34686bfec7784c431
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-22 14:37:48 +0200

    Add a menu sceen that animates in a title and a play button.

---
+++ game/Assets/Menu/PlayButton/DisableEnableOnPress.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableEnableOnPress : MonoBehaviour {
+
+	public GameObject toDisable;
+	public GameObject toEnable;
+
+	public void onPress() {
+		toDisable.SetActive (false);
+		toEnable.SetActive (true);
+	}
+
+}
