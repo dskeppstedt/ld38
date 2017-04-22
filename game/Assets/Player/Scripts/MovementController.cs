8ec816db157105946a31f3309781ab9527ee7ec4
Andreas Brommund <andreas@brommund.se>
2017-04-22 14:34:14 +0200

    Rename file

--- game/Assets/Player/Scripts/MovementController.cs
+++ game/Assets/Player/Scripts/MovementController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementComponents : MonoBehaviour {
+public class MovementController : MonoBehaviour {
 
 	public float acceleration;
 	public float maxSpeed;
