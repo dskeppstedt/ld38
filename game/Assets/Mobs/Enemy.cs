c1cfffa6b4e71528baa5f220a19ed75cadc2e5a4
Dan Hemgren <dhemgren@kth.se>
2017-04-22 19:05:48 +0200

    enemy spawner sketching

---
+++ game/Assets/Mobs/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour{
+
+	public MobType mobType;
+
+	private Transform defaultParent;
+
+	// Use this for initialization
+	void Start () {
+		defaultParent = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDisable(){
+		ReturnToPool ();
+	}
+
+	void ReturnToPool(){
+		transform.parent = defaultParent;
+	}
+}
