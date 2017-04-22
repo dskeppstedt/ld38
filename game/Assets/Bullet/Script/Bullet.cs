5003e97302d63eed49f1bf0dfa65d6a45ce6a288
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-22 21:08:21 +0200

    Add collision and explosion component till all bullets

--- game/Assets/Bullet/Script/Bullet.cs
+++ game/Assets/Bullet/Script/Bullet.cs
@@ -9,12 +9,14 @@ public class Bullet : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		this.gameObject.AddComponent<BulletCollision> ();
+		this.gameObject.AddComponent<Explosion> ();
+		Debug.Log ("Hello");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//TODO DESTORY 
 	}
-
 }
