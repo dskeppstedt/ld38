ab38f6826fc73077073ed08ef9a66d8a1bd1d5fb
Dan Hemgren <dhemgren@kth.se>
2017-04-22 15:29:42 +0200

    camera follow + background + main

---
+++ game/Assets/Camera/CameraFollowObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowObject : MonoBehaviour {
+
+	public Transform t;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (t.position.x, t.position.y, transform.position.z);
+	}
+}
