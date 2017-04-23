0e9696bc2247c9e06ed0f11b9e7fbf58d79842c8
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-23 17:35:39 +0200

    Add test game

---
+++ game/Assets/GameStatus/PlayerAlive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerAlive : MonoBehaviour {
+
+	public Energy player;
+
+	void Update () {
+		if (!player.hasEnergy()) {
+			Debug.Log ("Game Over");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
