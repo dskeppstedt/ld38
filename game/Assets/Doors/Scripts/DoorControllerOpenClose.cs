dd5e07cfb383509ab1b59f2fc531e5dadc2e46af
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-23 12:23:18 +0200

    Add door controllers that can open and close

---
+++ game/Assets/Doors/Scripts/DoorControllerOpenClose.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControllerOpenClose : DoorController,Opener,Closer {
+
+	void Update () {
+		if (trigger.active && Input.GetButtonDown("Action")) {
+			ActiveAllDoors ();
+		}
+	}
+
+	void ActiveAllDoors() {
+		foreach(Door d in doors){
+			if (d.open) Close (d); else Open (d);
+		}
+	}
+
+	public void Open(Door door){
+		door.OpenDoor ();
+	}
+
+	public void Close(Door door){
+		door.CloseDoor();
+	}
+}
