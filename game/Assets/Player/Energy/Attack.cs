ede852f4e788a24dacc8cac4ab884dd0f4351b9d
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-23 21:17:10 +0200

    Update world with new backdrop

--- game/Assets/Player/Energy/Attack.cs
+++ game/Assets/Player/Energy/Attack.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof (BoxCollider2D))]
+[RequireComponent (typeof (Collider2D))]
 public class Attack : MonoBehaviour {
 
 	public int damage;
