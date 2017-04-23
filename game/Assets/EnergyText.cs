2511167aafa993343e0ac338f5a24df209154850
David Skeppstedt <david.skeppstedt@gmail.com>
2017-04-23 15:07:10 +0200

    Add base for energy system

---
+++ game/Assets/EnergyText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//This shows the energy level of the player...
+public class EnergyText : MonoBehaviour {
+
+	Energy playerEnergy;
+	Text energyText;
+
+	// Use this for initialization
+	void Start () {
+		playerEnergy = GameObject.FindGameObjectWithTag ("Player").GetComponent<Energy> ();
+		if (playerEnergy == null) {
+			Debug.LogError ("Player has no Energy component");
+		}
+
+		energyText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		energyText.text = "Energy: " + playerEnergy.getEnergy ();
+	}
+}
---
+++ game/Assets/Player/Energy/Scripts/Energy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy : MonoBehaviour {
+
+	public int initialEnergy = 300;
+	int energy;
+
+	public bool alive;//TODO hide from editor
+
+	public void Start() {
+		this.energy = initialEnergy;
+	}
+
+	public bool ReduceEnergy(int amount) {
+		if (energy - amount < 0) {
+			return false;
+		}
+
+		energy -= amount;
+		return true;
+	}
+
+	public bool hasEnergy() {
+		return energy > 0;
+	}
+
+	public int getEnergy() {
+		return energy;
+	}
+
+	public void setEnergy(uint energy) {
+		this.energy = (int) energy;
+	}
+
+}
