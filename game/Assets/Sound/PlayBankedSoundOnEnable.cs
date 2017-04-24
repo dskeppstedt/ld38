e4bf60ebfea47ba3cb45233013c6ae851a824d87
Dan Hemgren <dhemgren@kth.se>
2017-04-24 19:19:50 +0200

    soundbank : handles max voices for sfx (explosions)

---
+++ game/Assets/Sound/PlayBankedSoundOnEnable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBankedSoundOnEnable : MonoBehaviour {
+
+	public SoundType sound;
+
+	void OnEnable(){
+		SoundBank.PlaySound (sound, transform.position);
+	}
+}
---
+++ game/Assets/Sound/SoundBank.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBank : MonoBehaviour {
+
+	const int MAX_VOICES = 5;
+
+	public AudioSource explSource;
+	private static AudioSource staticExplSource;
+	private static AudioSource[] explSourceBuffer;
+
+	void Start(){
+		staticExplSource = explSource;
+		InitBuffers ();
+	}
+
+	void InitBuffers(){
+		explSourceBuffer = new AudioSource[MAX_VOICES];
+		for (int i = 0; i < MAX_VOICES; i++) {
+			explSourceBuffer [i] = (Instantiate (explSource, this.transform)).GetComponent<AudioSource>();
+		}
+	}
+
+	static AudioSource FindFreeSource(SoundType sound){
+		switch (sound) {
+		case SoundType.Explosion:
+			for (int i = 0; i < explSourceBuffer.Length; i++) {
+				if (!explSourceBuffer [i].isPlaying) {
+					return explSourceBuffer [i];
+				}
+			}
+			return explSourceBuffer [Random.Range (0, explSourceBuffer.Length)];
+		default:
+			return null;
+		}
+	}
+
+	public static void PlayExplosion(Vector2 pos){
+		staticExplSource = FindFreeSource (SoundType.Explosion);
+		staticExplSource.transform.position = pos;
+		staticExplSource.time = 0f;
+		staticExplSource.Play ();
+	}
+
+	public static void PlaySound(SoundType sound, Vector2 pos){
+		switch (sound) {
+		case SoundType.Explosion:
+			PlayExplosion (pos);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+public enum SoundType { Explosion }
