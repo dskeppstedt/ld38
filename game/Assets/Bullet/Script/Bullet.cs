31f082215ac69b493e790c83319430479d910d83
Andreas Brommund <andreas@brommund.se>
2017-04-24 13:32:32 +0200

    Bouncing bullets

--- game/Assets/Bullet/Script/Bullet.cs
+++ game/Assets/Bullet/Script/Bullet.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
-
+	public bool bounce;
 	public float damage;
 	public float kickback;
+
+	private Vector2 oldVelocity;
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +18,14 @@ public class Bullet : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	void FixedUpdate () {
+		// because we want the velocity after physics, we put this in fixed update
+		oldVelocity = GetComponent<Rigidbody2D>().velocity;
+	}
+
+	public Vector2 GetOldVelocity(){
+		return oldVelocity;
 	}
 }
--- game/Assets/Bullet/Script/BulletCollision.cs
+++ game/Assets/Bullet/Script/BulletCollision.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class BulletCollision : MonoBehaviour {
 	
 	void OnCollisionEnter2D (Collision2D col){
-		this.gameObject.SetActive (false);
+		Bullet bullet = GetComponent<Bullet> ();
+
+		if (bullet.bounce && col.collider.tag == "Room") {
+			// get the point of contact
+			ContactPoint2D contact = col.contacts[0];
+
+			// reflect our old velocity off the contact point's normal vector
+			Vector2 reflectedVelocity = Vector2.Reflect(bullet.GetOldVelocity(), contact.normal);        
+
+			// assign the reflected velocity back to the rigidbody
+			GetComponent<Rigidbody2D>().velocity = reflectedVelocity;
+			// rotate the object by the same ammount we changed its velocity
+			Quaternion rotation = Quaternion.FromToRotation(bullet.GetOldVelocity(), reflectedVelocity);
+			transform.rotation = rotation * transform.rotation;
+		}else{
+			this.gameObject.SetActive (false);
+		}
+
 	}
 }
