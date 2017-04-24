6a3bec3f5afc2ce9625e8ea6cb01acb01753c130
Dan Hemgren <dhemgren@kth.se>
2017-04-24 21:56:03 +0200

    fade in music when entering corridor

---
+++ game/Assets/MusicTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicTrigger : MonoBehaviour {
+
+	public AudioSource src;
+	public AudioMixerSnapshot music;
+	public float fadeInTime;
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.tag == "Player"){
+			PlayMusic ();
+			gameObject.SetActive(false);
+		}
+	}
+
+	void PlayMusic(){
+		src.Play ();
+		music.TransitionTo (fadeInTime);
+	}
+}
